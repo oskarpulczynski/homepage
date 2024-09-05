a2218cc84c112f7e6c44d0e138e60799b64fa458
Oskar <o.pulczynski@gmail.com>
2024-09-05 18:42:06 +0200

    Added new page

--- main.js
+++ main.js
@@ -3,4 +3,4 @@ const age = 26
 
 console.log(firstName);
 console.log(age);
-console.log(`Nazywam się ${firstName}, mam ${age} lat i zmarnowałem ${age} lat życia`)
+console.log(`Hello World! Nazywam się ${firstName}, mam ${age} lat i zmarnowałem ${age} lat życia`)
