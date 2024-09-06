c615e849f9a2c32e3dc11608007ae55361827a1e
Oskar <o.pulczynski@gmail.com>
2024-09-06 17:05:04 +0200

    Styling and fixing

--- main.js
+++ main.js
@@ -3,12 +3,11 @@ const age = 26
 
 console.log(firstName);
 console.log(age);
-console.log(`Hello World! Nazywam się ${firstName}, mam ${age} lat i zmarnowałem ${age} lat życia`)
 
 const header = document.querySelector('.page-header__heading--js')
 
 console.log(header.innerHTML)
 
-header.innerHTML = 'Hej!'
+header.innerHTML = `Hello World! Nazywam się ${firstName}, mam ${age} lat i zmarnowałem ${age} lat życia`
 
 console.log(header.innerHTML)
