a8cd8d0ee8448f50c37e6f3e6948bdafd6f76824
Oskar <o.pulczynski@gmail.com>
2024-09-06 17:10:21 +0200

    Added text to HTML by JS code

--- main.js
+++ main.js
@@ -11,3 +11,7 @@ console.log(header.innerHTML)
 header.innerHTML = `Hello World! Nazywam się ${firstName}, mam ${age} lat i zmarnowałem ${age} lat życia`
 
 console.log(header.innerHTML)
+
+const hiddenText = document.querySelector('.page-header__text--js')
+
+hiddenText.innerHTML = 'Ten tekst widzisz tylko dzięki JS ;)'
