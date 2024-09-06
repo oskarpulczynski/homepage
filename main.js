8cd7d7490627404d45780d01cbf86c3c5e6ff7bd
Oskar <o.pulczynski@gmail.com>
2024-09-06 14:04:15 +0200

    Learning scripts and BEM

--- main.js
+++ main.js
@@ -4,3 +4,11 @@ const age = 26
 console.log(firstName);
 console.log(age);
 console.log(`Hello World! Nazywam się ${firstName}, mam ${age} lat i zmarnowałem ${age} lat życia`)
+
+const header = document.querySelector('.page-header__heading--js')
+
+console.log(header.innerHTML)
+
+header.innerHTML = 'Hej!'
+
+console.log(header.innerHTML)
