1f8d62796ec96d5eba768b6ca37a0b569e0ac457
Oskar <o.pulczynski@gmail.com>
2024-09-06 18:57:01 +0200

    typography playground

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-const firstName = 'Oskar'
-const age = 26
+const firstName = "Oskar";
+const age = 26;
 
 console.log(firstName);
 console.log(age);
 
-const header = document.querySelector('.page-header__heading--js')
+const header = document.querySelector(".page-header__heading--js");
 
-console.log(header.innerHTML)
+console.log(header.innerHTML);
 
-header.innerHTML = `Hello World! Nazywam się ${firstName}, mam ${age} lat i zmarnowałem ${age} lat życia`
+header.innerHTML = `Hello World! Nazywam się ${firstName}, mam ${age} lat i zmarnowałem ${age} lat życia`;
 
-console.log(header.innerHTML)
+console.log(header.innerHTML);
 
-const hiddenText = document.querySelector('.page-header__text--js')
+const hiddenText = document.querySelector(".page-header__text--js");
 
-hiddenText.innerHTML = 'Ten tekst widzisz tylko dzięki JS ;)'
+hiddenText.innerHTML = "Ten tekst widzisz tylko dzięki JS ;)";
