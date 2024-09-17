357b644ec8f59b856c279e813f7e35a3e63430be
Oskar <o.pulczynski@gmail.com>
2024-09-17 17:38:59 +0200

    Learning javascript Arrays

---
+++ src/js/arrays.js
@@ -0,0 +1,20 @@
+const hardware = ["mouse", "keyboard", "screen"];
+
+console.log(hardware.length);
+
+const myObject = {
+	name: "Maciek",
+	age: 35,
+};
+
+const myText = "WTF: Co ten frontend";
+
+const textLength = myText.length;
+
+console.log(myText[textLength - 1]);
+
+console.log(hardware)
+
+hardware.splice(0,3)
+
+console.log(hardware)
